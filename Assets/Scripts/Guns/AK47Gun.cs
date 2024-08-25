f7aafa2ceca8f6f98ce01b1e01d1a1b7d23b1bef
unknown <hoangbichht@gmail.com>
2024-08-25 15:34:10 +0700

    Add  ak47 overheat system

--- Assets/Scripts/Guns/AK47Gun.cs
+++ Assets/Scripts/Guns/AK47Gun.cs
@@ -1,13 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AK47Gun : Gun
 {
     [Header("AK47 settings")]
     [SerializeField] float minFireCD = 0.2f;
     [SerializeField] float baseFireCD = 0.75f;
+    [Tooltip("Time for the cd to change from base value to min value")]
     [SerializeField] float cdReduceTime = 2f;
+    [Space]
+    [SerializeField] Slider overheatBar;
+    [SerializeField] float overheatValue;
+    [SerializeField] float currentHeatValue;
+    [SerializeField] float heatPerShot;
+    [SerializeField] float cooldownTime;
+    [SerializeField] float minCooldownPower = 0.4f;
+    [SerializeField] bool overheated = false;
+    float cooldownPower; // use to make the effect when the more player shoot, the faster it cooldown
+    float deactiveTime;
+    [Space]
     [SerializeField] bool isShooting = false;
     float currentFireCD;
     float timer;
@@ -17,12 +30,20 @@ public class AK47Gun : Gun
     void OnEnable()
     {
         currentFireCD = baseFireCD;
-        timer = currentFireCD;
+        timer = currentFireCD; // get ready for first shot
+        overheatBar.gameObject.SetActive(true);
+
+        // calculate elapsed time from the deactive moment to update heat bar
+        float elapsedTime = Time.time - deactiveTime;
+        currentHeatValue -= elapsedTime * overheatValue / cooldownTime;
+        UpdateOverheatBar(currentHeatValue / overheatValue);
     }
 
     void Update()
     {
-        if (!isShooting) return;
+        Cooldown();
+
+        if (!isShooting || overheated) return;
 
         if (timer < currentFireCD)
         {
@@ -30,6 +51,37 @@ public class AK47Gun : Gun
         }
 
         CheckTimer();
+        CheckOverheat();
+    }
+
+    void CheckOverheat()
+    {
+        if (currentHeatValue >= overheatValue)
+        {
+            overheated = true;
+            cooldownPower = 1f;
+        }
+    }
+
+    void Cooldown()
+    {
+        // cooldown power is 1 mean the gun is overheat so no need to calculate cooldown power
+        if (!Mathf.Approximately(cooldownPower, 1f)) {
+            // calculate cooldown power base on current heat value, the higher heat value, the faster cooldown
+            cooldownPower = Mathf.Max(minCooldownPower, currentHeatValue / overheatValue);
+        }
+
+        // cooldown base on cooldown time, power and overheat value
+        currentHeatValue -= Time.deltaTime * (overheatValue / cooldownTime) * cooldownPower;
+        currentHeatValue = Mathf.Max(currentHeatValue, 0f); // keep it higher than 0f
+        UpdateOverheatBar(currentHeatValue / overheatValue);
+
+        // turn off overheat
+        if (currentHeatValue <= 0f)
+        {
+            overheated = false;
+            cooldownPower = minCooldownPower;
+        }
     }
 
     void CheckTimer()
@@ -38,7 +90,14 @@ public class AK47Gun : Gun
         {
             timer = 0f;
             Shoot(rotation);
+            currentHeatValue += heatPerShot; // increase overheat bar per shot
+            UpdateOverheatBar(currentHeatValue / overheatValue);
         }
+    }
+
+    void UpdateOverheatBar(float percent)
+    {
+        overheatBar.value = percent;
     }
 
     IEnumerator CR_ReduceCD()
@@ -77,11 +136,18 @@ public class AK47Gun : Gun
         timer = baseFireCD;
 
         isShooting = false;
-        
+
         if (reduceCDCoroutine != null)
         {
             StopCoroutine(reduceCDCoroutine);
             reduceCDCoroutine = null;
         }
+    }
+
+    void OnDisable()
+    {
+        deactiveTime = Time.time;
+        
+        overheatBar.gameObject?.SetActive(false);
     }
 }
